571a09ba88b1df495a858cd10044bc707ccb1d06
Enel Almonte <enel_almonte@ucne.edu.do>
2024-09-23 19:02:56 -0400

    Contexto agregado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,9 @@
+﻿using EnelAlmonte_Ap1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnelAlmonte_Ap1_P1.DAL;
+
+public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
+{
+    public DbSet<Registros> Registros { get; set; }
+}
---
+++ Models/Registros.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnelAlmonte_Ap1_P1.Models;
+
+public class Registros
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Nombre { get; set; }
+}
